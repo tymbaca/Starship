3d6862752cdbba4cac9ba14abc1827cae431457d
tymbaca <tumb4ka228@gmail.com>
2023-05-07 18:55:45 +0200

    Death menu fade interpolation. Better retry icon

--- scenes/Level/DeathMenu.gd
+++ scenes/Level/DeathMenu.gd
@@ -2,10 +2,13 @@ extends Control
 class_name DeathMenu
 
 @export var player: Player
+@onready var background := $Background
+@onready var retry_button := $Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.player_dead.connect(visibility_on)
+	modulate.a = 0
 	pass # Replace with function body.
 
 
@@ -14,4 +17,6 @@ func _process(delta: float) -> void:
 	pass
 	
 func visibility_on():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 1, 1) 
 	visible = true
